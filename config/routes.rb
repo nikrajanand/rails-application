a1b27f682c79d4ec7f1cb91815803cc0e09da33b
nikrajanand <nikraj92@ymail.com>
2017-05-08 20:17:04 +1000

    route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'sessions/new'
+
   root 'static_pages#home'
   get '/contact-us', to:'static_pages#contact'
   get '/about-us', to: 'static_pages#about'
   get '/helloworld', to: 'static_pages#helloworld'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
